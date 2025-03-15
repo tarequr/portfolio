ee6bde1cce5a76145df4d0af38acc7d87dfd7123
tarequr <tarequrr8@gmail.com>
2025-03-15 21:57:00 +0600

    feat: remove tailwind 4 and include 3

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
