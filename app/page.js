ce8723fb5a267fddfa6315836a07551720537988
tarequr <tarequrr8@gmail.com>
2025-03-18 22:20:48 +0600

    core: about section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Navbar />
       <Header />
+      <About />
     </>
   );
 }
