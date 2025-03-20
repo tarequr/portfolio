ee1bfb788223815b8e86af50bb97b20be80f57a6
tarequr <tarequrr8@gmail.com>
2025-03-20 23:53:43 +0600

    core: complete my work section

---
+++ app/components/Work.jsx
@@ -0,0 +1,40 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Work = () => {
+    return (
+        <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
+            <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+
+            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                Welcome to my web development portfolio! Explore a collection of projects
+                showcasing my expertise in front-end development.
+            </p>
+
+            <div className='grid grid-cols-auto my-10 gap-5'>
+                {workData.map(({ title, description, bgImage }, index) => (
+                    <div key={index} style={{ backgroundImage: `url(${bgImage})` }} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
+                        <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                            <div>
+                                <h2 className='font-semibold'>{title}</h2>
+                                <p className='text-sm text-gray-700'>{description}</p>
+                            </div>
+
+                            <div class='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                                <Image src={assets.send_icon} alt='Send Icon' className='w-5' />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <a href="" class="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500">
+                Show more <Image src={assets.right_arrow_bold} alt="Right arrow" class="w-4" />
+            </a>
+        </div>
+    )
+}
+
+export default Work
