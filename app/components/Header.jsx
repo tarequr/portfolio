5e76698dc9134c0d70fc3df59868c11a54b1900d
tarequr <tarequrr8@gmail.com>
2025-03-24 22:53:33 +0600

    chore: my resume button chanege on dark mode

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -15,12 +15,12 @@ const Header = () => {
             <p className='max-w-2xl max-auto font-Ovo'>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
 
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-                <a href="#contact" className='px-10 py-3 border  border-white rounded-full bg-black text-white flex items-center gap-2'>
+                <a href="#contact" className='px-10 py-3 border  border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
                     Contact Me
                     <Image src={assets.right_arrow_white} alt='Contact' className='w-4' />
                 </a>
 
-                <a href="/sample-resume.pdf" download className='px-10 py-3 border  rounded-full border-gray-500 flex items-center gap-2'>
+                <a href="/sample-resume.pdf" download className='px-10 py-3 border  rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                     My Resume
                     <Image src={assets.download_icon} alt='' className='w-4' />
                 </a>
