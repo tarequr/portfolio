635e101cbdc59ebbc8bc35188541bfd87c450885
tarequr <tarequrr8@gmail.com>
2025-04-07 21:53:38 +0600

    chore: contact section is complete for dark mode

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -28,17 +28,8 @@ const Contact = () => {
         }
     };
 
-
-
-
-
-
-
-
-
-
     return (
-        <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+        <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
             <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
 
@@ -53,14 +44,14 @@ const Contact = () => {
                         name='name'
                         placeholder='Enter your name'
                         required
-                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
                     />
                     <input
                         type='email'
                         name='email'
                         placeholder='Enter your email'
                         required
-                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
                     />
                 </div>
                 <textarea
@@ -68,12 +59,12 @@ const Contact = () => {
                     name='message'
                     placeholder='Enter your message'
                     required
-                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'
+                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90'
                 ></textarea>
 
                 <button
                     type='submit'
-                    className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'
+                    className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
                 >
                     Submit now <Image src={assets.right_arrow_white} alt='Arrow' className='w-4' />
                 </button>
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = ({isDarkMode}) => {
+const Work = ({ isDarkMode }) => {
     return (
         <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
