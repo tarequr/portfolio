b665a4a4aeb9b744a2498fbef17e520733ac7ac1
tarequr <tarequrr8@gmail.com>
2025-04-18 23:57:23 +0600

    chore: motion of contact section is done

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,6 +2,8 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React, { useState } from 'react'
 
+import { motion } from "motion/react"
+
 const Contact = () => {
     const [result, setResult] = useState("");
 
@@ -29,24 +31,56 @@ const Contact = () => {
     };
 
     return (
-        <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
-            <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'
+        >
+            <motion.h4 
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className='text-center mb-2 text-lg font-Ovo'
+            >Connect with me</motion.h4>
 
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+            <motion.h2 
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+                className='text-center text-5xl font-Ovo'
+            >Get in touch</motion.h2>
+
+            <motion.p 
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.7, duration: 0.5 }}
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+            >
                 I'd love to hear from you! If you have any questions, comments, or feedback, please use the from below.
-            </p>
+            </motion.p>
 
-            <form className='max-w-2xl mx-auto' onSubmit={onSubmit}>
+            <motion.form
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.9, duration: 0.5 }} 
+                className='max-w-2xl mx-auto' onSubmit={onSubmit}
+            >
                 <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-                    <input
+                    <motion.input
+                        initial={{ opacity: 0, x: -50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 1.1, duration: 0.6 }}
                         type='text'
                         name='name'
                         placeholder='Enter your name'
                         required
                         className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
                     />
-                    <input
+                    <motion.input
+                        initial={{ opacity: 0, x: 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ delay: 1.2, duration: 0.6 }}
                         type='email'
                         name='email'
                         placeholder='Enter your email'
@@ -54,24 +88,29 @@ const Contact = () => {
                         className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
                     />
                 </div>
-                <textarea
+                <motion.textarea
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 1.3, duration: 0.6 }}
                     rows='6'
                     name='message'
                     placeholder='Enter your message'
                     required
                     className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90'
-                ></textarea>
+                ></motion.textarea>
 
-                <button
+                <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    transition={{ duration: 0.3 }}
                     type='submit'
                     className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
                 >
                     Submit now <Image src={assets.right_arrow_white} alt='Arrow' className='w-4' />
-                </button>
+                </motion.button>
 
                 <p className='mt-4'>{result}</p>
-            </form>
-        </div>
+            </motion.form>
+        </motion.div>
     )
 }
 
