79d126c13a651f93c6c73dfaaf3a7f6ab74be5e3
tarequr <tarequrr8@gmail.com>
2025-04-18 23:39:49 +0600

    chore: motion of work section is done

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,20 +2,52 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
+import { motion } from "motion/react"
+
 const Work = ({ isDarkMode }) => {
     return (
-        <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
-            <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            id='work' className='w-full px-[12%] py-10 scroll-mt-20'
+        >
+            <motion.h4 
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className='text-center mb-2 text-lg font-Ovo'
+            >My Portfolio</motion.h4>
 
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+            <motion.h2 
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+                className='text-center text-5xl font-Ovo'
+            >My Services</motion.h2>
+
+            <motion.p 
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.7, duration: 0.5 }}
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+            >
                 Welcome to my web development portfolio! Explore a collection of projects
                 showcasing my expertise in front-end development.
-            </p>
+            </motion.p>
 
-            <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
+            <motion.div 
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.9, duration: 0.6 }}
+                className='grid grid-cols-auto my-10 gap-5 dark:text-black'
+            >
                 {workData.map(({ title, description, bgImage }, index) => (
-                    <div key={index} style={{ backgroundImage: `url(${bgImage})` }} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
+                    <motion.div 
+                        whileHover={{ scale: 1.05 }}
+                        transition={{ duration: 0.3 }}
+                        key={index} style={{ backgroundImage: `url(${bgImage})` }} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+                    >
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                             <div>
                                 <h2 className='font-semibold'>{title}</h2>
@@ -26,14 +58,19 @@ const Work = ({ isDarkMode }) => {
                                 <Image src={assets.send_icon} alt='Send Icon' className='w-5' />
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
 
-            <a href="" class="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:hover:bg-darkHover">
+            <motion.a 
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 1.1, duration: 0.5 }}
+                href="" class="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:hover:bg-darkHover"
+            >
                 Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt="Right arrow" class="w-4" />
-            </a>
-        </div>
+            </motion.a>
+        </motion.div>
     )
 }
 
